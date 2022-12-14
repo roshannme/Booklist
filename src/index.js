0b41d6b1ec1c8e1e97fdd283eb285989b423193b
Cypher <mrcyphernp@gmail.com>
2022-12-14 18:49:06 +0545

    final booklisting

--- src/index.js
+++ src/index.js
@@ -19,41 +19,18 @@ import ReactDom from 'react-dom'
 //props
 // props child
 // Refactoring
+//Key prop
+// user events
+// Import export
+
 
 //css
 import './index.css'
 
+import {books} from './books'
+import Book from './Book'
+
 //setup vars
-const books = [
-{
-          img:'https://m.media-amazon.com/images/I/51Zu0ZwT0jL._SX320_BO1,204,203,200_.jpg',
-          title:'It Ends with Us',
-          author:'hoover'
-        
-        },
-
-{
-          img:'https://m.media-amazon.com/images/I/51-uspgqWIL._SX329_BO1,204,203,200_.jpg',
-          title:'Atomic Habits',
-          author:'James'
-        
-        },
-
-{
-          img:'https://m.media-amazon.com/images/I/41zFito0fdL._SX373_BO1,204,203,200_.jpg',
-          title:'Gotodinner',
-          author:'hardcover'
-        
-        },
-
-        {
-          img:'https://m.media-amazon.com/images/I/518z5dvykPL._SX348_BO1,204,203,200_.jpg',
-          title:'Intresting facts about minds',
-          author:'Jordan'
-        
-        },
-
-      ];
 
 
         function BookList(){
@@ -61,7 +38,7 @@ const books = [
             <section className='bookname'>{books.map((book)=>{
               const{img,title,author} = book;
               return(
-              <Book book={book}> </Book>
+              <Book book key={book.id} book={book}> </Book>
 
               );
             })}
@@ -71,18 +48,6 @@ const books = [
         }
 
 
-      //  const Book = ({img,title,author}) =>{
-        const Book = (props)=>{
-        const {img,title,author} = props.book;
 
-        return (
-        <article className='book'>
-            <img src={img} alt=''/>
-            <h1>{title}</h1>
-            <h4>{author}</h4>
-            
-        </article>
-        );
-       };
        
         ReactDom.render(<BookList/>,document.getElementById('root'));
