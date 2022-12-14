43014cfd788b29cc58de4411d5993042a9518223
Cypher <mrcyphernp@gmail.com>
2022-12-14 18:47:20 +0545

    final booklisting

---
+++ src/Book.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+            //  const Book = ({img,title,author}) =>{
+       const Book = (props)=>{
+        const {img,title,author} = props.book;
+          // attribute,event handler
+          //onClick,onMouseOver
+        return (
+        <article className='book'>
+            <img src={img} alt=''/>
+            <h1>{title}</h1>
+            <h4>{author}</h4>
+            
+        </article>
+        );
+       };
+
+export default Book
---
+++ src/books.js
@@ -0,0 +1,33 @@
+export const books = [
+{          
+    
+    id:1,
+          img:'https://m.media-amazon.com/images/I/51Zu0ZwT0jL._SX320_BO1,204,203,200_.jpg',
+          title:'It Ends with Us',
+          author:'hoover'
+        
+        },
+
+{       
+    id:2,
+          img:'https://m.media-amazon.com/images/I/51-uspgqWIL._SX329_BO1,204,203,200_.jpg',
+          title:'Atomic Habits',
+          author:'James'
+        
+        },
+
+{         id:3,
+          img:'https://m.media-amazon.com/images/I/41zFito0fdL._SX373_BO1,204,203,200_.jpg',
+          title:'Gotodinner',
+          author:'hardcover'
+        
+        },
+
+        { id:4,
+          img:'https://m.media-amazon.com/images/I/518z5dvykPL._SX348_BO1,204,203,200_.jpg',
+          title:'Intresting facts about minds',
+          author:'Jordan'
+        
+        },
+
+      ];
